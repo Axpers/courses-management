a73f4de3ee5dc212c03e8335f8fd706295066931
Garik Derminjyan <20193733@etud.univ-evry.fr>
2021-06-17 11:36:22 +0200

    Suppresion des imports inutilisés

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,8 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material/material.module';
-import {CoursesComponent} from './courses/courses.component';
 import {SidebarComponent} from './sidebar/sidebar.component';
 import {HeaderComponent} from './header/header.component';
-import {ListCoursesComponent} from './courses/list-courses/list-courses.component';
-import {CourseFormComponent} from './courses/course-form/course-form.component';
-import {CoursesListService} from './shared/courses-list.service';
 import {FormsModule} from '@angular/forms';
 import {SidebarRoutesService} from './shared/sidebar-routes.service';
 
