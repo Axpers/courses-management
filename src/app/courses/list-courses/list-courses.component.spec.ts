2d23d95e46366438df8f7d649aa8ecf05ff11dd1
GarikD <garikd@outlook.fr>
2022-02-03 19:08:42 +0100

    updated deps

--- src/app/courses/list-courses/list-courses.component.spec.ts
+++ src/app/courses/list-courses/list-courses.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ListCoursesComponent } from './list-courses.component';
 
@@ -6,7 +6,7 @@ describe('ListCoursesComponent', () => {
   let component: ListCoursesComponent;
   let fixture: ComponentFixture<ListCoursesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ListCoursesComponent ]
     })
